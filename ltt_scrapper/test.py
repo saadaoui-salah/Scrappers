e65ca8ff07b194493630f5fae5ac42f851ebbdaa
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-09-26 12:51:24 -0700

    add tests

---
+++ ltt_scrapper/test.py
@@ -0,0 +1,31 @@
+from ltt import send_request, get_data 
+
+##############################
+#   GET DATA TEST
+##############################
+get_data()
+
+##############################
+#   SEND DATA TEST
+##############################
+send_request(
+    zip_="somelhfslfkh",
+    lastName="somelhfslfkh",
+    country="somelhfslfkh",
+    occupation="somelhfslfkh",
+    city="somelhfslfkh",
+    courseID="AAEP3E20A-02",
+    privacyPolicy="true",
+    sendNews="true",
+    firstName="somelhfslfkh",
+    places="somelhfslfkh",
+    phone="somelhfslfkh",
+    taxId="somelhfslfkh",
+    street="somelhfslfkh",
+    company="somelhfslfkh",
+    comment="somelhfslfkh",
+    captcha="65c56",
+    salutation="Mr.",
+    email="somelhfslfkh@gmail.com",
+    title="_AAEP3E20A__-_Airbus_A300-600__PW_4000__vs_Airbus_A300-600__any_Engine__EASA_Part-66_B1___B2_Practical"
+     )
